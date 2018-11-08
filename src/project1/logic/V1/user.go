7f1504c95f529e39d3fd00d7b174aa12c3b438ca
xiaoqi <l>
2018-11-08 18:37:27 +0800

    demo redis local update to 127.0.0.1

--- src/project1/logic/V1/user.go
+++ src/project1/logic/V1/user.go
@@ -72,15 +72,15 @@ func Myjson(ctx iris.Context) {
 
 func Rget(ctx iris.Context) {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "127.0.0.1:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	err := client.Set("go_tt", "hello,world!", 0).Err()
+	/*err := client.Set("go_tt", "hello,world!", 0).Err()
 	if err != nil {
 		panic(err)
-	}
+	}*/
 
 	val, err := client.Get("go_tt").Result()
 	ctx.JSON(iris.Map{
